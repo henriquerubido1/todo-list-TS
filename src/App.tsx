14e2dc472c400c83597f2806b1a8d3a892b8830e
henriqueRubido <henriquerubido1@gmail.com>
2022-05-11 18:46:09 -0300

    todo list setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+				<h1>Todo List in TypeScript</h1>
+			</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      'green': '#13ce66',
+      'blue': '#1fb6ff',
+      'red': '#e11d48',
+      'yellow': '#ffc82c',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'slate': '#0f172a',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#94a3b8'
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
